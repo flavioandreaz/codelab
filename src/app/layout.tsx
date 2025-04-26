402a1a1f7532d59a2e2aacb6d7e84041a474c0e9
Gabriel Borges <contato@gbdev.me>
2025-04-26 15:52:34 -0300

    feat: adicionando o shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 
 import "@/styles/globals.css";
+import { cn } from "@/lib/utils";
 
 const nunito = Nunito({
   variable: "--font-sans",
@@ -18,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={`${nunito.variable} antialiased font-sans`}>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={cn(nunito.variable, "antialiased font-sans dark")}>
         <h1>LAYOUT</h1>
         {children}
       </body>
