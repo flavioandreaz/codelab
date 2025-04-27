74a08c6ec1e41a9fdce157a7e392bee3fa959749
Gabriel Borges <contato@gbdev.me>
2025-04-26 21:20:04 -0300

    feat: listagem de cursos e tags

--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,10 +1,32 @@
-import { Button } from "@/components/ui/button";
+import { CoursesList } from "@/components/pages/courses/courses-list";
+import { CourseTagsList } from "@/components/pages/courses/tags-list";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
 
-export default function Home() {
+type CoursesPageProps = {
+  searchParams: Promise<{
+    query: string;
+    tags: string | string[];
+  }>;
+};
+
+export default async function CoursesPage({ searchParams }: CoursesPageProps) {
+  const { query, tags } = await searchParams;
+
+  const suspenseKey = JSON.stringify({ query, tags });
+
   return (
     <>
-      <h1>CodeLab</h1>
-      <Button>Click me</Button>
+      <Suspense
+        key={`tags-${suspenseKey}`}
+        fallback={<Skeleton className="w-full h-[22px] min-h-[22px]" />}
+      >
+        <CourseTagsList />
+      </Suspense>
+
+      <Suspense key={suspenseKey} fallback={<Skeleton className="flex-1" />}>
+        <CoursesList query={query} tags={tags} />
+      </Suspense>
     </>
   );
 }
--- src/components/shared/search-input.tsx
+++ src/components/shared/search-input.tsx
@@ -1,9 +1,55 @@
+"use client";
+
+import { Search } from "lucide-react";
 import { Input } from "../ui/input";
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/use-debounce";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 
 export const SearchInput = () => {
+  const [value, setValue] = useState("");
+
+  const debouncedValue = useDebounce(value);
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const currentTags = searchParams.getAll("tags");
+  const currentQuery = searchParams.get("query");
+
+  useEffect(() => {
+    if (currentQuery === debouncedValue) return;
+    if (!currentQuery && !debouncedValue) return;
+
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          tags: currentTags,
+          query: debouncedValue,
+        },
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(url);
+  }, [currentQuery, currentTags, debouncedValue, pathname, router]);
+
   return (
     <div className="flex-1 max-w-[400px] relative">
-      <Input className="h-9" placeholder="Busque por um curso" />
+      <Input
+        className="h-9 pl-9 peer"
+        placeholder="Busque por um curso"
+        value={value}
+        onChange={({ target }) => setValue(target.value)}
+      />
+
+      <Search
+        className="absolute top-1/2 -translate-y-1/2 left-3 text-muted-foreground peer-focus:text-primary transition-all"
+        size={16}
+      />
     </div>
   );
 };
