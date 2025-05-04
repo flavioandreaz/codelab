1439d66180c547c524f7cf486807ee7ba7e3aaa4
Gabriel Borges <contato@gbdev.me>
2025-05-04 12:43:36 -0300

    feat: criando popup de notificações

--- src/components/shared/app-sidebar/nav-user.tsx
+++ src/components/shared/app-sidebar/nav-user.tsx
@@ -21,10 +21,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useClerk, useUser } from "@clerk/nextjs";
 import { BadgeCheck, ChevronsUpDown, LogIn, LogOut } from "lucide-react";
 import Link from "next/link";
+import { NotificationsPopover } from "./notifications-popover";
+import { cn } from "@/lib/utils";
 
 export const NavUser = () => {
   const { user, isLoaded } = useUser();
-  const { isMobile } = useSidebar();
+  const { isMobile, open } = useSidebar();
 
   const { openUserProfile, signOut } = useClerk();
 
@@ -32,32 +34,18 @@ export const NavUser = () => {
     <SidebarMenu>
       <SidebarMenuItem>
         {user ? (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <SidebarMenuButton
-                size="lg"
-                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-              >
-                <Avatar src={user.imageUrl} fallback={user.fullName} />
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">
-                    {user.fullName}
-                  </span>
-                  <span className="truncate text-xs">
-                    {user.primaryEmailAddress?.emailAddress}
-                  </span>
-                </div>
-                <ChevronsUpDown className="ml-auto size-4" />
-              </SidebarMenuButton>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              className="w-[var(--radix-dropdown-menu-trigger-width)] min-w-56 rounded-lg"
-              side={isMobile ? "bottom" : "right"}
-              align="end"
-              sideOffset={4}
-            >
-              <DropdownMenuLabel className="p-0 font-normal">
-                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+          <div
+            className={cn(
+              "flex items-center gap-2",
+              !open && "flex-col-reverse"
+            )}
+          >
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton
+                  size="lg"
+                  className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
                   <Avatar src={user.imageUrl} fallback={user.fullName} />
                   <div className="grid flex-1 text-left text-sm leading-tight">
                     <span className="truncate font-semibold">
@@ -67,24 +55,47 @@ export const NavUser = () => {
                       {user.primaryEmailAddress?.emailAddress}
                     </span>
                   </div>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem onClick={() => openUserProfile()}>
-                  <BadgeCheck />
-                  Gerenciar conta
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem onClick={() => signOut()}>
-                  <LogOut />
-                  Sair
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
+                  <ChevronsUpDown className="ml-auto size-4" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="w-[var(--radix-dropdown-menu-trigger-width)] min-w-56 rounded-lg"
+                side={isMobile ? "bottom" : "right"}
+                align="end"
+                sideOffset={4}
+              >
+                <DropdownMenuLabel className="p-0 font-normal">
+                  <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                    <Avatar src={user.imageUrl} fallback={user.fullName} />
+                    <div className="grid flex-1 text-left text-sm leading-tight">
+                      <span className="truncate font-semibold">
+                        {user.fullName}
+                      </span>
+                      <span className="truncate text-xs">
+                        {user.primaryEmailAddress?.emailAddress}
+                      </span>
+                    </div>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem onClick={() => openUserProfile()}>
+                    <BadgeCheck />
+                    Gerenciar conta
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem onClick={() => signOut()}>
+                    <LogOut />
+                    Sair
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <NotificationsPopover />
+          </div>
         ) : (
           <>
             {!isLoaded ? (
